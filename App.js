17860f7a0a11323e4374d686f648916e33fd1fc9
Thuan Tieu <Thuan.Tieu@metropolia.fi>
2019-08-29 19:36:39 +0300

    add .eslintrc.js

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+
+const mediaArray = [
+  {
+    'key': '0',
+    'title': 'Title 1',
+    // eslint-disable-next-line max-len
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/161',
+    },
+    'filename': 'http://placekitten.com/2048/1920',
+  },
+  {
+    'key': '1',
+    'title': 'Title 2',
+    // eslint-disable-next-line max-len
+    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/162',
+    },
+    'filename': 'http://placekitten.com/2041/1922',
+  },
+  {
+    'key': '2',
+    'title': 'Title 3',
+    // eslint-disable-next-line max-len
+    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/163',
+    },
+    'filename': 'http://placekitten.com/2039/1920',
+  },
+];
+
+// eslint-disable-next-line react/jsx-no-undef
+<FlatList
+  data={mediaArray}
+  renderItem={({item}) => {
+    return (
+      // eslint-disable-next-line react/jsx-no-undef
+      <TouchableOpacity>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: item.thumbnails.w160}}
+        />
+        <View>
+          <Text>{item.title}</Text>
+          <Text>{item.description}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }}
+/>;
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +72,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
